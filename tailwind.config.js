68d974403c714ece2764aba92b65f5e6a8938a0f
TheRealToxicDev <toxic.dev09@gmail.com>
2022-02-01 10:20:04 -0700

    feat(update): Tailwind Components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
   theme: {
     extend: {
       backgroundImage: {
