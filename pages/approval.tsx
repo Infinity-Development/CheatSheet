e570db00346a94991ed2c6484b935fe35fea1d6f
TheRealToxicDev <toxic.dev09@gmail.com>
2022-02-01 15:17:28 -0700

    feat(add): Approval Reasons

--- pages/approval.tsx
+++ pages/approval.tsx
@@ -1,7 +1,7 @@
 import Footer from "../components/footer";
 import copy from 'copy-to-clipboard';
 import toast, { Toaster } from 'react-hot-toast';
-import denyFeatures from "../components/denial/features";
+import approveFeatures from "../components/approval/features";
 
 export default function DenialPage() {
   return (
@@ -9,14 +9,14 @@ export default function DenialPage() {
       <div className="px-4 py-16 mx-auto sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24 lg:px-8">
       <h1 className="text-center text-7xl font-extrabold tracking-tighter leading-[1.1] sm:text-8xl lg:text-9xl xl:text-9xl">
           <span className="future inline-block text-transparent bg-clip-text bg-gradient-radial from-color2 to-color2 via-color1 animate-gradient-x bg-repeat">
-            Denial.
+            Approval.
           </span>{' '}
       </h1>
       <p className="max-w-lg mx-auto mt-6 text-3xl font-medium leading-tight text-center text-gray-400 sm:max-w-4xl sm:text-2xl md:text-3xl lg:text-4xl">
-          Pre-Configured list of Bot Denial Reasons
+          Pre-Configured list of Bot Approval Reasons
       </p>
         <div className="grid grid-cols-1 mt-12 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-12">
-          {denyFeatures.map((feature) => (
+          {approveFeatures.map((feature) => (
             <div
               className="p-10 w-auto bg-slate-800 shadow-lg rounded-xl dark:bg-opacity-5 "
               key={feature.name}
